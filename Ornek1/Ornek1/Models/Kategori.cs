1e3927d0181b34279feedbee15d34c691fcbd721
musaturkan <musa.turkan@bilecik.edu.tr>
2023-11-09 17:51:36 +0300

    View component eklendi. Kategori entitisi eklendi

---
+++ Ornek1/Ornek1/Models/Kategori.cs
@@ -0,0 +1,9 @@
+﻿namespace Ornek1.Models
+{
+    public class Kategori
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string KisaAd { get; set; }
+    }
+}
---
+++ Ornek1/Ornek1/ViewComponents/KategoriViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ornek1.ViewComponents;
+
+    public class KategoriViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            List<Models.Kategori> liste = new();
+            liste.Add(new Models.Kategori { Id = 1, Ad = "Elektronik", KisaAd = "El" });
+            liste.Add(new Models.Kategori { Id = 2, Ad = "Mobilya", KisaAd = "Mob" });
+            liste.Add(new Models.Kategori { Id = 3, Ad = "Tekstil", KisaAd = "Tks" });
+            liste.Add(new Models.Kategori { Id = 4, Ad = "Spor", KisaAd = "Spr" });
+            
+
+            return View("kategoriListe",liste);
+        } 
+    }
